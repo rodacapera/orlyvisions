2f17886b95b7477d775ad68f1351240e78e8ca46
Jorge Eliecer Rojas Quiñones <jerojas@famisanar.com.co>
2022-07-28 11:15:43 -0500

    estilos

--- landing_page/src/components/navigation.jsx
+++ landing_page/src/components/navigation.jsx
@@ -25,11 +25,11 @@ export const Navigation = (props) => {
           id='bs-example-navbar-collapse-1'
         >
           <ul className='nav navbar-nav navbar-right'>
-            <li>
+            {/* <li>
               <a href='#features' className='page-scroll'>
                 Features
               </a>
-            </li>
+            </li> */}
             <li>
               <a href='#about' className='page-scroll'>
                 About
@@ -40,7 +40,7 @@ export const Navigation = (props) => {
                 Services
               </a>
             </li>
-            <li>
+            {/* <li>
               <a href='#portfolio' className='page-scroll'>
                 Gallery
               </a>
@@ -49,7 +49,7 @@ export const Navigation = (props) => {
               <a href='#testimonials' className='page-scroll'>
                 Testimonials
               </a>
-            </li>
+            </li> */}
             <li>
               <a href='#team' className='page-scroll'>
                 Team
