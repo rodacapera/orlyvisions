66bfa678783f06c2e59ebf9ee981f71b2bb58b31
Jorge Eliecer Rojas Quiñones <jerojas@famisanar.com.co>
2022-07-28 16:01:03 -0500

    estilos

--- landing_page/src/components/contact.jsx
+++ landing_page/src/components/contact.jsx
@@ -150,9 +150,9 @@ export const Contact = (props) => {
       <div id='footer'>
         <div className='container text-center'>
           <p>
-            &copy; 2020 Issaaf Kattan React Land Page Template. Design by{' '}
+            &copy; 2022 Panic Button. Design by{' '}
             <a href='http://www.templatewire.com' rel='nofollow'>
-              TemplateWire
+              Desarrollos Teranov
             </a>
           </p>
         </div>
