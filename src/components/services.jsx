ea1b50b0b9a090f2b7e2908445c06084ade4ade3
Jorge Eliecer Rojas Quiñones <jerojas@famisanar.com.co>
2022-08-01 10:33:11 -0500

    cambios en el data

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -5,8 +5,7 @@ export const Services = (props) => {
         <div className='section-title'>
           <h2>Our Services</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+            Conoce los servicios disponibles que ofrecemos
           </p>
         </div>
         <div className='row'>
