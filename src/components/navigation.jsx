fbf6e9a9da4156bea9ac6a6573e65437e6bfbe63
Jorge Eliecer Rojas Quiñones <jerojas@famisanar.com.co>
2022-08-01 10:45:39 -0500

    agregar las imagenes

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -40,12 +40,12 @@ export const Navigation = (props) => {
                 Services
               </a>
             </li>
-            {/* <li>
+            <li>
               <a href='#portfolio' className='page-scroll'>
                 Gallery
               </a>
             </li>
-            <li>
+            {/* <li>
               <a href='#testimonials' className='page-scroll'>
                 Testimonials
               </a>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,8 @@ import { Header } from "../components/header";
 /* import { Features } from "./components/features"; */
 import { About } from "../components/about";
 import { Services } from "../components/services";
-/* import { Gallery } from "./components/gallery";
+import { Gallery } from "../components/gallery";
+/*
 import { Testimonials } from "./components/testimonials"; */
 import { Team } from "../components/Team";
 import SmoothScroll from "smooth-scroll";
@@ -28,8 +29,8 @@ function Home() {
       {/* <Features data={landingPageData.Features} /> */}
       <About data={landingPageData.About} />
       <Services data={landingPageData.Services} />
-      {/* <Gallery data={landingPageData.Gallery}/>
-        <Testimonials data={landingPageData.Testimonials} /> */}
+      <Gallery data={landingPageData.Gallery}/>
+       {/* <Testimonials data={landingPageData.Testimonials} /> */}
       <Team data={landingPageData.Team} />
       <Contact data={landingPageData.Contact} />
     </div>
