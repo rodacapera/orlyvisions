e3851442641120b555057f979f68f908ed64ccf8
Jorge Eliecer Rojas Quiñones <jerojas@famisanar.com.co>
2022-08-05 10:58:36 -0500

    cambio en la disposicion de la imagen

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -14,7 +14,7 @@ export const Gallery = (props) => {
           <div className='portfolio-items'>
             {props.data
               ? props.data.map((d, i) => (
-                <div key={`${d.title}-${i}`} className='col-sm-6 col-md-4 col-lg-4'>
+                <div key={`${d.title}-${i}`} className='col-sm-12 col-md-6 col-lg-4'>
                   <Image  title={d.title} text={d.text} /* largeImage={d.largeImage} */ smallImage={d.smallImage} />
                 </div>
               ))
