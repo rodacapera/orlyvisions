5b46cdb3c56dc7c90f2bf47d78177815b264234f
Jorge Eliecer Rojas Quiñones <jerojas@famisanar.com.co>
2022-08-06 08:59:27 -0500

    cambuo en la importacion

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ import { Services } from "../components/services";
 import { Gallery } from "../components/gallery";
 /*
 import { Testimonials } from "./components/testimonials"; */
-import { Team } from "../components/Team";
+/* import { Team } from "../components/Team"; */
 import SmoothScroll from "smooth-scroll";
 import { Contact } from "../components/contact";
 import JsonData from "../data/data.json";
