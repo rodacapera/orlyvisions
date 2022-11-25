d6c9bf6a8476cc730901758079ea32f73d719b4b
roquijo <roquijo@hotmail.com>
2022-11-25 12:48:46 -0500

    mensaje-video

--- src/components/bikeAlert.jsx
+++ src/components/bikeAlert.jsx
@@ -12,23 +12,27 @@ export const BikeAlert = (props) => {
         </div>
         <div className="container">
           <div className="row">
-            <div  className="col-xs-12 col-md-6 ">
+            <div className="col-xs-12 col-md-6 ">
               <iframe id='video-responsive' src="https://www.youtube.com/embed/9utqxeHobkU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
             <div className="col-xs-12 col-md-6">
-              <div className="about-text">
+              <div className="about-text" style={{paddingBottom: '50px'}}>
                 <h2>{t("bikeAlert.about")}</h2>
-                <p style={{textAlign: 'justify'}}>{t("bikeAlert.paragraph1")}</p>
-                <h3>{t("bikeAlert.how")}</h3>
-                <p style={{textAlign: 'justify'}}>{t("bikeAlert.paragraph2")}</p>
+                <p style={{ textAlign: 'justify' }}>{t("bikeAlert.paragraph1")}</p>
               </div>
             </div>
           </div>
         </div>
-        <div>
-          <div>
-            <div className='col-xs-12 col-md-12'>
-              <iframe id='video'src="https://www.youtube.com/embed/feM65He3v98" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-12 col-md-6 ">
+              <iframe id='video-responsive' src="https://www.youtube.com/embed/feM65He3v98" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <div className="col-xs-12 col-md-6">
+              <div className="about-text" style={{paddingBottom: '50px'}}>
+                <h2>{t("bikeAlert.how")}</h2>
+                <p style={{ textAlign: 'justify' }}>{t("bikeAlert.paragraph2")}</p>
+              </div>
             </div>
           </div>
         </div>
